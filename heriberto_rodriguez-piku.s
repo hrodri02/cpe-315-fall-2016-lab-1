2a6f8b87c4e98c117896e80c895b0d786ff5aa75
hrodri02 <hrodri02@calpoly.edu>
2016-09-26 15:26:38 -0700

    Add files via upload

---
+++ heriberto_rodriguez-piku.s
@@ -0,0 +1,35 @@
+   @ Programmer: Heriberto Rodriguez
+   @       Date: 9/26/2016
+   @      Class: CPE 315
+
+   .syntax unified
+   @ ------------------------
+   .global main
+main:
+   @ Stack the return address (lr) in addition to dummy register (ip) to
+   @ keep the stack 8-byte aligned.
+   push  {ip, lr}
+
+   @ Load the arguments and perform the calls.
+   ldr   r0, =line1  @ add string in line1 to r0
+   bl    printf      @ print r0
+   ldr   r0, =line2  @ add string in line2 to r0
+   bl    printf      @ print r0
+   ldr   r0, =line3  @ add string in line3 to r0
+   bl    printf      @ print r0
+
+   @ Exit from 'main'. This is like 'return 0' in C.
+   mov   r0, #0   @ Return 0.
+   @ Pop the dummy ip to reserve our alignment fix, and pop the original lr
+   @ value directly into pc - the Program Counter - to return.
+   pop   {ip, pc}
+
+   @ ------------------------
+   @ Data for the printfs. The GNU assembler's ".asciz" directive
+   @ automatically adds a NULL character termination.
+line1:
+   .asciz   "oh raspberry pi\n"
+line2:
+   .asciz   "I am afriad to use you\n"
+line3:
+   .asciz   "don't be difficult\n"
